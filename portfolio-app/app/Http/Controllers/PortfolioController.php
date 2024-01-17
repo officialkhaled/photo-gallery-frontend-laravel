d3d6edb74c11e87fcfd9350cd771a6d8cef7c7dc
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2024-01-17 21:00:48 +0600

    files require relocating

--- portfolio-app/app/Http/Controllers/PortfolioController.php
+++ portfolio-app/app/Http/Controllers/PortfolioController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PortfolioController extends Controller
 {
     public function index()
     {
         return view('content');
     }
-    
+
     public function about()
     {
         return view('about');
